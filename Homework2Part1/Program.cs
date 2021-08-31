ff594240649b822542ab61980ad1f6d9e9dae7aa
Plutosgit <venkataraman_balaji@hotmail.com>
2021-08-31 11:31:52 -0700

    Add project files.

---
+++ Homework2Part1/Program.cs
@@ -0,0 +1,104 @@
+﻿
+
+/// Homework Week No. 2		Exercise No. 1
+/// Solution Name: Homework2Part1
+/// @author: Balaji Venkataraman
+/// Date:  August 30, 2021
+///
+/// Problem Statement: Output the pig latin name to the screen, given a first and last name.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for 2 strings (first and last name)
+/// 3) Create the PigLatin name 
+/// 4) Print the PigLatin to the screen
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2Part1
+{
+    class MyFirstProgram
+    {
+        static void Main(string[] args)
+        {
+            // declare two string variables for first & last names and a string variable for the final output in Piglatin
+            string sFirstname, sLastname;
+            string sPLname = "", sPL1 = "", sPL2 = "";
+            string userInput;
+
+            // print a message to the screen & receive the 2 string inputs
+            Console.WriteLine("Week 2; Homework 1");
+            Console.WriteLine("Hello out there. This HW shows string manipulation, by");
+            Console.WriteLine("creating PigLatin names from given first & last names.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("Enter the first name in lowercase & press [Enter]: ");
+            userInput = Console.ReadLine();
+            sFirstname = userInput.ToLower();
+
+            Console.Write("Enter the  last name in lowercase & press [Enter]: ");
+            userInput = Console.ReadLine();
+            sLastname = userInput.ToLower();
+
+            // create the PigLatin name
+            // process firstname
+            switch (sFirstname.Length)
+            {
+
+                case 0:
+                    Console.WriteLine("The first name must not be empty. Cannot form PigLatin name.");
+                    break;
+                case 1:
+                    sPL1 = sFirstname.ToUpper() + "ay";
+                    break;
+                case 2:
+                    sPL1 = sFirstname.Substring(1, 1).ToUpper() + sFirstname.Substring(0, 1) + "ay";
+                    break;
+                default:
+                    sPL1 = sFirstname.Substring(1, 1).ToUpper() + sFirstname.Substring(2, sFirstname.Length - 2) + sFirstname.Substring(0, 1) + "ay";
+                    break;
+            }
+
+            // process lastname
+            switch (sLastname.Length)
+            {
+
+                case 0:
+                    Console.WriteLine("The last  name must not be empty. Cannot form PigLatin name.");
+                    break;
+                case 1:
+                    sPL2 = sLastname.ToUpper() + "ay";
+                    break;
+                case 2:
+                    sPL2 = sLastname.Substring(1, 1).ToUpper() + sLastname.Substring(0, 1) + "ay";
+                    break;
+                default:
+                    sPL2 = sLastname.Substring(1, 1).ToUpper() + sLastname.Substring(2, sLastname.Length - 2) + sLastname.Substring(0, 1) + "ay";
+                    break;
+            }
+
+            // now combine to form the Piglatin name
+            sPLname = sPL1 + " " + sPL2;
+
+            // print the output message with the Piglatin name to the screen
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Output");
+            Console.WriteLine("======");
+            Console.WriteLine("Your first and last names are: " + sFirstname + ", " + sLastname);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("The PigLatin equivalent is   : " + sPLname);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Homework2Part2/Program.cs
@@ -0,0 +1,77 @@
+﻿/*using System;
+
+namespace Homework2Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+*/
+
+
+/// Homework Week No. 2		Exercise No. 1
+/// Solution Name: Homework2Part1
+/// @author: Balaji Venkataraman
+/// Date:  August 30, 2021
+///
+/// Problem Statement: Convert Fahrenheit to Celsius 
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for a temperature in deg F as a whole number 
+/// 3) Calculate the temperature equivalent in deg C 
+/// 4) Print the output message to the screen
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2Part2
+{
+    class MyFirstProgram
+    {
+        static void Main(string[] args)
+        {
+            // declare an integer variable for deg F (userinput)
+            int ndegF;
+            double ndegC;
+            string userInput;
+
+            // print a message to the screen & receive input
+            Console.WriteLine("Week 2; Homework 2");
+            Console.WriteLine("Hello there. This HW shows arithematic expressions, by");
+            Console.WriteLine("converting a given temperature in deg F to deg C.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("Enter the temperature in degree F as a whole number & press [Enter]: ");
+            
+            userInput = Console.ReadLine();
+            ndegF = Int32.Parse(userInput);
+
+            // calcualte the deg C equivalent
+            ndegC = 5 * ((double) ndegF - 32) / 9;
+
+            // print the output message & output the deg C to screen
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Output");
+            Console.WriteLine("======");
+            Console.WriteLine("Your  input temperature in deg F is: " + ndegF );
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Your output temperature in deg C is: " + Math.Round(ndegC,1));
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ReadLine();
+
+        }
+    }
+}
